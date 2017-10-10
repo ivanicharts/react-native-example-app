35a649357f81e72aa5e12c5253d3c6e498a82ccf
Ivan Botnari <ivan.botnari@titanium-soft.com>
2017-10-10 17:52:08 +0300

    Styled header

--- src/containers/HomeScreen/index.js
+++ src/containers/HomeScreen/index.js
@@ -1,16 +1,45 @@
 import React, { PureComponent } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+
+const s = StyleSheet.create({
+  header: {
+    backgroundColor: '#3A3748'
+  },
+  title: {
+    color: 'white',
+    fontWeight: '300'
+  },
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    alignItems: 'center'
+  },
+  search: {
+    height: 40,
+    width: '100%',
+    backgroundColor: '#f2f2f2',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10
+  }
+})
 
 class HomeScreen extends PureComponent {
   static navigationOptions = {
-    title: 'Home'
+    title: 'Chat list',
+    headerStyle: s.header,
+    headerTitleStyle: s.title
   }
 
   render = () => (
-    <View>
+    <View style={s.container}>
+      <View style={s.search}>
+        <Text>Search</Text>
+      </View>
       <Text>Home screen</Text>
     </View>
   )
 }
+
 
 export default HomeScreen
